af537ae0316af89912c227bfe97b16b29827c279
Jason Lee <chsienlee@gmail.com>
2015-09-08 23:39:26 +0800

    Tweak example

--- example/postalcode/App.js
+++ example/postalcode/App.js
@@ -7,17 +7,17 @@ import Town            from './stores/Town'
 
 var { $store, $action } = Wuwei('postalcodeApp');
 
+// Set store & stores's path
+$store.create('dataSource', DataSource)
+$store.create('county', County).source('dataSource');
+$store.create('town', Town).source('dataSource', 'county');
+
+// load JSON
+$store.dataSource.load();
+
 export default class App extends Component {
   constructor() {
     super();
-
-    // Set store & stores's path
-    $store.create('dataSource', DataSource)
-    $store.create('county', County).source('dataSource');
-    $store.create('town', Town).source('dataSource', 'county');
-
-    // load JSON
-    $store.dataSource.load();
 
     this.state = $store.subscribe({
         'county': 'county',
