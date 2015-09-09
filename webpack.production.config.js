c9a4cb630b3ddb384771e5239077cc6583cd4529
Jason Lee <chsienlee@gmail.com>
2015-09-10 03:00:17 +0800

    Update example

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 module.exports = {
   devtool: 'eval',
   entry: [
-    './example/todo/index'
+    './example/index'
   ],
   output: {
     path: path.join(__dirname, 'dist'),
